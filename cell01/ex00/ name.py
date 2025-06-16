eb5e68a0520532c04054b01d71aa59ca70402c2c
praypriya2301 <praypriyabuyngam@gmail.com>
2025-06-16 09:18:27 +0000

    name

---
+++ cell01/ex00/ name.py
@@ -0,0 +1,4 @@
+ first_name = "praypriya"
+last_name = "bungam"
+print(first_name " " +last_name)
+print(f"{first_name} {last_name}") // format string
---
+++ cell01/ex02/  age.py
@@ -0,0 +1,3 @@
+age = 18
+to_add = 442
+my_age = a
