8f90dc2435687dd374a8d85030397eb5e2906700
praypriya2301 <praypriyabuyngam@gmail.com>
2025-06-17 02:56:31 +0000

    name.py

--- cell01/ex00/ name.py
+++ cell01/ex00/ name.py
@@ -1,4 +1,3 @@
- first_name = "praypriya"
+first_name = "praypriya"
 last_name = "bungam"
-print(first_name " " +last_name)
-print(f"{first_name} {last_name}") // format string
+print(first_name,last_name)
